600eaed2d5336b8f6a19f3bdd5332c8696ace7d7
gys <nova_gaoys@126.com>
2020-04-28 11:07:54 +0800

    提交

--- backend/config/mongo.js
+++ backend/config/mongo.js
@@ -5,7 +5,7 @@ const config = {
   // url: "mongodb://localhost:27017",
   // database: 'file',
   // collection: 'test',
-  url: "mongodb://novafiles_user:123456@dds-bp1fbee6363228e41642.mongodb.rds.aliyuncs.com:3717,dds-bp1fbee6363228e42114.mongodb.rds.aliyuncs.com:3717/novafiles?replicaSet=mgset-4767543",
+ 
   database: 'novafiles',
   collection: 'file',
   useNewUrlParser: true
